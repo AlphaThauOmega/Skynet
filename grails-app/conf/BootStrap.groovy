5c5cb32ee638b9ce94bd9dd02ddf3369f4055870
mijail <mijail@teotl>
2015-05-14 17:36:17 -0500

    fixe bugs remove

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -8,7 +8,7 @@ class BootStrap {
                                                     nombre: 'mijail',
                                                     correo: 'mijail@mijail.com',
                                                     contrasena:'318672fb86ed60eb2a230a782d53f93c243d199f6f6972fee17a0ce8591ec803f0abf83335b2777b1c44792f98cf66567109c843a1c0deaa2a26b85825ca5ee7');
-            admin.save();
+            admin.save(flus:true);
     }
     def destroy = {
     }
